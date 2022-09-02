679e471c19fd0a2036b83136ce0a17311278c9ce
AlterEgoAdj <asjoshi3198@gmail.com>
2022-09-02 11:46:28 +0530

    Utils function

--- insertrandom/models.py
+++ insertrandom/models.py
@@ -12,87 +12,11 @@ class Author(models.Model):
     class Meta:
         app_label = "insertrandom"
 
-    @staticmethod
-    def random_object():
-        objectlist = Author.objects.all()
-        idlist = []
-        for i in objectlist:
-            idlist.append(i.id)
-        if not idlist:
-            return None
-        else:
-            return random.choice(idlist)
-
-    def random_data(self):
-        randict = {}
-        for f in self._meta.fields:
-            if type(f) == (CharField or TextField):
-                randict[f.name] = r_char()
-            elif type(f) == (IntegerField or BigIntegerField or PositiveIntegerField):
-                randict[f.name] = r_int()
-            elif type(f) == FloatField:
-                randict[f.name] = r_float()
-            elif type(f) == EmailField:
-                randict[f.name] = r_email()
-            elif type(f) == DateTimeField:
-                randict[f.name] = r_past_date()
-            elif type(f) == URLField:
-                randict[f.name] = r_url()
-            elif type(f) == ForeignKey:
-                randict[f.name] = f.related_model.random_object()
-            elif type(f) == (BigAutoField or AutoField):
-                continue
-            elif type(f) == BinaryField:
-                randict[f.name] = r_binary()
-            elif type(f) == (BooleanField or NullBooleanField):
-                randict[f.name] = r_bool()
-            elif type(f) == UUIDField:
-                randict[f.name] = r_uuid()
-        return randict
-
 
 class Publisher(models.Model):
     publisher_name = models.CharField(max_length=255)
     publisher_email = models.EmailField(max_length=255)
     publisher_site = models.URLField(max_length=255)
-
-    @staticmethod
-    def random_object():
-        objectlist = Author.objects.all()
-        idlist = []
-        for i in objectlist:
-            idlist.append(i.id)
-        if not idlist:
-            return None
-        else:
-            return random.choice(idlist)
-
-    def random_data(self):
-        randict = {}
-        for f in self._meta.fields:
-            if type(f) == (CharField or TextField):
-                randict[f.name] = r_char()
-            elif type(f) == (IntegerField or BigIntegerField or PositiveIntegerField):
-                randict[f.name] = r_int()
-            elif type(f) == FloatField:
-                randict[f.name] = r_float()
-            elif type(f) == EmailField:
-                randict[f.name] = r_email()
-            elif type(f) == DateTimeField:
-                randict[f.name] = r_past_date()
-            elif type(f) == URLField:
-                randict[f.name] = r_url()
-            elif type(f) == ForeignKey:
-                randict[f.name] = f.related_model.random_object()
-            elif type(f) == (BigAutoField or AutoField):
-                continue
-            elif type(f) == BinaryField:
-                randict[f.name] = r_binary()
-            elif type(f) == (BooleanField or NullBooleanField):
-                randict[f.name] = r_bool()
-            elif type(f) == UUIDField:
-                randict[f.name] = r_uuid()
-        return randict
 
 
 class Books(models.Model):
@@ -109,30 +33,3 @@ class Books(models.Model):
             "author",
             "book_isbn",
         )
-
-    def random_data(self):
-        randict = {}
-        for f in self._meta.fields:
-            if type(f) == (CharField or TextField):
-                randict[f.name] = r_char()
-            elif type(f) == (IntegerField or BigIntegerField or PositiveIntegerField):
-                randict[f.name] = r_int()
-            elif type(f) == FloatField:
-                randict[f.name] = r_float()
-            elif type(f) == EmailField:
-                randict[f.name] = r_email()
-            elif type(f) == DateTimeField:
-                randict[f.name] = r_past_date()
-            elif type(f) == URLField:
-                randict[f.name] = r_url()
-            elif type(f) == ForeignKey:
-                randict[f.name] = f.related_model.random_object()
-            elif type(f) == (BigAutoField or AutoField):
-                continue
-            elif type(f) == BinaryField:
-                randict[f.name] = r_binary()
-            elif type(f) == (BooleanField or NullBooleanField):
-                randict[f.name] = r_bool()
-            elif type(f) == UUIDField:
-                randict[f.name] = r_uuid()
-        return randict
--- insertrandom/utils.py
+++ insertrandom/utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date, timedelta
 from django.db.models.fields import *
 from django.db.models.fields.related import *
 import random
@@ -6,37 +6,47 @@ import string
 import uuid
 
 
-def r_char():
-    return "".join(random.choices(string.ascii_letters, k=16))
+def r_char(n):
+    return "".join(random.choices(string.ascii_letters, k=random.randrange(n)))
 
 
-def r_int():
-    return random.randrange(10000)
+def r_int(n=10000):
+    return random.randrange(n)
 
 
-def r_float():
-    return random.uniform(0.0, 10000.0)
+def r_float(n=10000.0, m=0.0):
+    return random.uniform(m, n)
 
 
 def r_email():
     return (
-        "".join(random.choices(string.ascii_letters, k=5))
+        "".join(random.choices(string.ascii_letters, k=random.randrange(10)))
         + "@"
-        + "".join(random.choices(string.ascii_letters, k=5))
+        + "".join(random.choices(string.ascii_letters, k=random.randrange(10)))
         + ".com"
     )
 
 
-def r_past_date():
-    return datetime.now() - random.randrange(10000)
+def r_datetime(start=datetime.strptime("1970-1-1", "%Y-%m-%d"), end=datetime.now()):
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = random.randrange(int_delta)
+    return start + timedelta(seconds=random_second)
+
+
+def r_date(start=date(1970, 1, 1), end=date.today()):
+    delta = start - end
+    int_delta = delta.days
+    random_day = random.randrange(int_delta)
+    return start + timedelta(days=random_day)
 
 
 def r_url():
     return (
         "https://www."
-        + "".join(random.choices(string.ascii_letters, k=10))
+        + "".join(random.choices(string.ascii_letters, k=random.randrange(10)))
         + ".com/"
-        + "".join(random.choices(string.ascii_letters, k=6))
+        + "".join(random.choices(string.ascii_letters, k=random.randrange(6)))
     )
 
 
@@ -52,40 +62,32 @@ def r_uuid():
     return str(uuid.uuid4())
 
 
-# The function below is supposed to be added to model classes
-
-
-def random_data(ClassName):
-    randict = {}
-    for f in ClassName._meta.fields:
-        if type(f) == (CharField or TextField):
-            randict[f.name] = r_char()
-        elif type(f) == (IntegerField or BigIntegerField or PositiveIntegerField):
-            randict[f.name] = r_int()
-        elif type(f) == FloatField:
-            randict[f.name] = r_float()
-        elif type(f) == EmailField:
-            randict[f.name] = r_email()
-        elif type(f) == DateTimeField:
-            randict[f.name] = r_past_date()
-        elif type(f) == URLField:
-            randict[f.name] = r_url()
-        elif type(f) == ForeignKey:
-            randict[f.name] = f.related_model.random_object()
-        elif type(f) == (BigAutoField or AutoField):
-            continue
-        elif type(f) == BinaryField:
-            randict[f.name] = r_binary()
-        elif type(f) == (BooleanField or NullBooleanField):
-            randict[f.name] = r_bool()
-        elif type(f) == UUIDField:
-            randict[f.name] = r_uuid()
-    return randict
-
-
-def testing(ClassName, n=1):
+def random_data(ClassName, n=1):
     masterlist = []
     for i in range(n):
-        obj = ClassName()
-        masterlist.append(obj.random_data())
+        randict = {}
+        for f in ClassName._meta.fields:
+            if type(f) == (CharField or TextField):
+                randict[f.name] = r_char(f.max_length)
+            elif type(f) == (IntegerField or BigIntegerField or PositiveIntegerField):
+                randict[f.name] = r_int()
+            elif type(f) == FloatField:
+                randict[f.name] = r_float()
+            elif type(f) == EmailField:
+                randict[f.name] = r_email()
+            elif type(f) == DateTimeField:
+                randict[f.name] = r_datetime()
+            elif type(f) == URLField:
+                randict[f.name] = r_url()
+            elif type(f) == ForeignKey:
+                randict[f.name] = None
+            elif type(f) == (BigAutoField or AutoField):
+                continue
+            elif type(f) == BinaryField:
+                randict[f.name] = r_binary()
+            elif type(f) == (BooleanField or NullBooleanField):
+                randict[f.name] = r_bool()
+            elif type(f) == UUIDField:
+                randict[f.name] = r_uuid()
+        masterlist.append(random_data())
     return masterlist
